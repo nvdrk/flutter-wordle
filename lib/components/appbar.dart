46d2f58e60959801b106e7545b51a0b467e0eaaa
steffen <steffen.hild@motion-s.com>
2023-07-02 17:43:23 +0200

    invalidate provider

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_wordle/game/game_provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_wordle/theme/style.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key, required this.title, this.isPop = false});
   final String title;
   final bool isPop;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return AppBar(
       shadowColor: greyTint.shade900,
       centerTitle: true,
@@ -23,6 +25,7 @@ class CustomAppBar extends StatelessWidget {
         icon: const Icon(Icons.arrow_back),
         onPressed: () {
           context.go('/');
+          ref.invalidate(gameProvider);
         },
       ) : const SizedBox.shrink(),
       title: Text(
